06bdc2846670b2fe0840c04a2c87b8415546293f
alachhman <33943685+alachhman@users.noreply.github.com>
2020-05-30 22:49:42 -0400

    fixed start.js

--- commands/start.js
+++ commands/start.js
@@ -5,11 +5,10 @@ module.exports = {
     display: 'start',
     description: 'sets person that typed command as a user`',
     async execute(message, args, client, db) {
-
         await db.collection('users').doc(message.author.id).set({
-            'userID': message.author.id,
-            'userName' : message.author.username,
-        })
+            'userId': message.author.id,
+            'userName': message.author.username
+        });
         message.channel.send("Welcome To DiscordMon");
     }
 };
