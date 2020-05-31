093c9d172a7e4f7c6fb28fb41fe7cecbe5fe8eb8
alachhman <33943685+alachhman@users.noreply.github.com>
2020-05-30 20:12:18 -0400

    Added pokemon spawning

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const {prefix, prodToken, stagingToken, environment} = require('./auth.json');
 const client = new Discord.Client();
+const Pokedex = require('pokedex-promise-v2');
+const P = new Pokedex();
 
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -20,6 +22,13 @@ client.on('message', async (message) => {
         message.reply("no problem fam");
         return;
     }
+
+    if(message.author.bot){return;}
+
+    if (Math.random() < 0.1) {
+        await spawnPokemon(message);
+    }
+
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -33,3 +42,24 @@ client.on('message', async (message) => {
 });
 
 client.login(environment === "production" ? prodToken : stagingToken);
+
+spawnPokemon = async (message) => {
+    let pkmn = Math.round(Math.random() * 1000) - 36;
+    if(pkmn < 1){pkmn = Math.abs(pkmn);}
+    console.log(pkmn);
+    let data;
+    await P.getPokemonByName(pkmn, function(response, error) { // with callback
+        if(!error) {
+            console.dir(response, {depth: 10});
+            data = response;
+        } else {
+            console.log(error);
+        }
+    });
+
+    const embed = new Discord.MessageEmbed()
+        .setTitle('A wild ' + data.forms[0].name + ' has appeared!')
+        .setColor("#3a50ff")
+        .setImage(data.sprites.front_default);
+    await message.channel.send({embed});
+};
