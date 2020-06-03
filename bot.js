7571586a1bd9107a11ed6708c896210d8eaf87fc
alachhman <33943685+alachhman@users.noreply.github.com>
2020-06-03 01:35:41 -0400

    added stats to pokemon

--- bot.js
+++ bot.js
@@ -48,7 +48,7 @@ client.on('message', async (message) => {
                 return;
             }
 
-            if (Math.random() < 0.1 && !await db.collection('guilds').doc(message.guild.id).get().then(x => x._fieldsProto.hasSpawn.booleanValue)) {
+            if (Math.random() < 0.3 && !await db.collection('guilds').doc(message.guild.id).get().then(x => x._fieldsProto.hasSpawn.booleanValue)) {
                 await spawnPokemon(message);
             }
 
@@ -73,6 +73,8 @@ client.on('guildCreate', async gData => {
 client.login(environment === "production" ? prodToken : stagingToken);
 
 spawnPokemon = async (message) => {
+    await updateGuild(message, "hasSpawn", true);
+
     let pkmn = Math.round(Math.random() * 1000) - 36;
     if (pkmn < 1) {
         pkmn = Math.abs(pkmn);
@@ -81,20 +83,22 @@ spawnPokemon = async (message) => {
     let data;
     await P.getPokemonByName(pkmn, function (response, error) { // with callback
         if (!error) {
-            //console.dir(response, {depth: 10});
             data = response;
         } else {
             console.log(error);
         }
     });
-    console.log(data.forms[0].name);
-    const embed = new Discord.MessageEmbed()
-        .setTitle('A wild ' + data.forms[0].name + ' has appeared!')
-        .setColor("#3a50ff")
-        .setImage(data.sprites.front_default);
-    await message.channel.send({embed});
-
-    await updateGuild(message, "hasSpawn", true);
+    try {
+        console.log(data.forms[0].name);
+        const embed = new Discord.MessageEmbed()
+            .setTitle('A wild ' + data.forms[0].name + ' has appeared!')
+            .setColor("#3a50ff")
+            .setImage(data.sprites.front_default);
+        await message.channel.send({embed});
+    } catch (e) {
+        await updateGuild(message, "hasSpawn", false);
+        return;
+    }
 
     const filter = m => m.content.toUpperCase() === data.forms[0].name.toUpperCase();
     const collector = message.channel.createMessageCollector(filter, {time: 15000});
@@ -105,31 +109,39 @@ spawnPokemon = async (message) => {
     });
 
     collector.on('end', async collected => {
-        if(collected.size > 0){
+        if (collected.size > 0) {
             let size = 0;
-            db.collection('users')
+            await db.collection('users')
                 .doc(collected.first().author.id)
                 .collection('pokemon')
                 .get().then(async snap => {
                 size = snap.size + 1;
-                const sizeS = size.toString();
-                await db.collection('users').doc(collected.first().author.id).collection('pokemon').doc(sizeS).set({
-                    'pokeID': size,
-                    'pokeName': data.forms[0].name,
-                    'pokeNickname': data.forms[0].name,
-                    'pokeApiEntry': data.forms[0].url
-                });
-                message.channel.send(collected.first().author.username + " has caught the pokemon!");
             });
+            let generatedPKMNData = await generatePKMN(data);
+            await db.collection('users').doc(collected.first().author.id).collection('pokemon').doc(size.toString()).set({
+                'pokeID': size,
+                'pokeName': data.forms[0].name,
+                'pokeNickname': data.forms[0].name,
+                'pokeApiEntry': data.forms[0].url,
+                'level': generatedPKMNData.level,
+                'nature': generatedPKMNData.nature,
+                'ivs': generatedPKMNData.ivs,
+                'stats': generatedPKMNData.stats
+            });
+            const embed = new Discord.MessageEmbed()
+                .setTitle(collected.first().author.username + ' has caught the ' + data.forms[0].name + '!')
+                .setColor("#38b938")
+                .setThumbnail(collected.first().author.avatarURL())
+                .setImage(data.sprites.front_default);
+            await message.channel.send({embed});
         }
         await updateGuild(message, "hasSpawn", false);
         message.channel.send("*DEBUG:* message collector end");
-    })
+    });
 };
 
 
 /**
- * work in progress function **IGNORE**
  * @returns {Promise<void>}
  */
 updateGuild = async (message, keyToChange, newValue) => {
@@ -142,11 +154,92 @@ updateGuild = async (message, keyToChange, newValue) => {
         'hasSpawn': false,
         'prefix': '>'
     };
-    for(let key in options){
-        if(!options.hasOwnProperty(key)) return;
-        if(key === keyToChange){
+    for (let key in options) {
+        if (!options.hasOwnProperty(key)) return;
+        if (key === keyToChange) {
             options[key] = newValue;
         }
     }
     db.collection('guilds').doc(message.guild.id).set(options);
 };
+
+
+generatePKMN = async (pkmn) => {
+    let level = await randomNum(60);
+    let nature = natures[await randomNum(natures.length)-1];
+    let ivs = {
+        "hp": await randomNum(31),
+        "attack": await randomNum(31),
+        "defense": await randomNum(31),
+        "special-attack": await randomNum(31),
+        "special-defense": await randomNum(31),
+        "speed": await randomNum(31),
+    };
+    let stats = {
+        hp: await generateStat(pkmn.stats, "hp", ivs, level, nature),
+        atk: await generateStat(pkmn.stats, "attack", ivs, level, nature),
+        def: await generateStat(pkmn.stats, "defense", ivs, level, nature),
+        spatk: await generateStat(pkmn.stats, "special-attack", ivs, level, nature),
+        spdef: await generateStat(pkmn.stats, "special-defense", ivs, level, nature),
+        speed: await generateStat(pkmn.stats, "speed", ivs, level, nature)
+    };
+    return {
+        level: level,
+        nature: nature,
+        ivs: ivs,
+        stats: stats
+    };
+};
+
+generateStat = async (stats, statToGen, ivs, level, nature) => {
+    let bs = 0;
+
+    let natureFactor = 1;
+    if (nature[1] === statToGen) {
+        natureFactor = 1.1;
+    } else if (nature[2] === statToGen) {
+        natureFactor = 0.9;
+    }
+
+    for (let stat of stats) {
+        if (stat.stat.name !== statToGen) continue;
+        bs = stat.base_stat;
+    }
+    if (statToGen === "hp") {
+        return Math.floor((2 * bs + ivs[statToGen] + 0) * level / 100 + level + 10);
+    } else {
+        return Math.floor(Math.floor((2 * bs + ivs[statToGen] + 0) * level / 100 + 5) * natureFactor)
+    }
+};
+
+randomNum = async (max) => {
+    return Math.floor(Math.random() * Math.floor(max)) + 1;
+};
+
+const natures = [
+    ['Hardy', 'None', 'None'],
+    ['Lonely', 'attack', 'defense'],
+    ['Brave', 'attack', 'speed'],
+    ['Adamant', 'attack', 'special-attack'],
+    ['Naughty', 'attack', 'special-defense'],
+    ['Docile', 'None', 'None'],
+    ['Bold', 'defense', 'attack'],
+    ['Relaxed', 'defense', 'speed'],
+    ['Impish', 'defense', 'special-attack'],
+    ['Lax', 'defense', 'special-defense'],
+    ['Serious', 'None', 'None'],
+    ['Timid', 'speed', 'attack'],
+    ['Hasty', 'speed', 'defense'],
+    ['Jolly', 'speed', 'special-attack'],
+    ['Naive', 'speed', 'special-defense'],
+    ['Bashful', 'None', 'None'],
+    ['Modest', 'special-attack', 'attack'],
+    ['Mild', 'special-attack', 'defense'],
+    ['Quiet', 'special-attack', 'speed'],
+    ['Rash', 'special-attack', 'special-defense'],
+    ['Quirky', 'None', 'None'],
+    ['Calm', 'special-defense', 'attack'],
+    ['Gentle', 'special-defense', 'defense'],
+    ['Sassy', 'special-defense', 'speed'],
+    ['Careful', 'special-defense', 'special-attack']
+];
