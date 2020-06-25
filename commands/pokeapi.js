34948c95fb024b76e594c30a4aed894ba5c61618
alachhman <33943685+alachhman@users.noreply.github.com>
2020-06-24 22:57:19 -0400

    added logging for pokemon level and evolve level

--- commands/pokeapi.js
+++ commands/pokeapi.js
@@ -23,8 +23,11 @@ module.exports = {
         const species = await axios.get(data.species.url);
         const evolutionChain = await axios.get(species.data.evolution_chain.url);
         message.channel.send("pulling data for " + pkmn.pokeName);
-        // console.dir(evolutionChain.data, {depth: 10})
-        console.dir(findPokemonInChain(evolutionChain.data.chain, pkmn.pokeName), {depth: 10})
+        const evoData = findPokemonInChain(evolutionChain.data.chain, pkmn.pokeName);
+        console.dir(evoData, {depth:10});
+        if(evoData.evolves_to[0].evolution_details[0].trigger.name === "level-up"){
+            message.channel.send("This pokemon needs to be level " + evoData.evolves_to[0].evolution_details[0].min_level + " to evolve, and it is currently level " + pkmn.level)
+        }
     }
 };
 
